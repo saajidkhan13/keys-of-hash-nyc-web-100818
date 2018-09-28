0ce679f24ef707a03feb29731b64654794a4459d
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-28 17:50:14 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,12 @@
 class Hash
   def keys_of(arguments)
     # code goes here
+    array = [] 
+    self.each do |k, v|
+      if arguments.include?(v)
+        array << key 
+      end 
+    end 
+    return array
   end
 end
